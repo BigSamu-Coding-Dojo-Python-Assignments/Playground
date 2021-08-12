0f7af5dc6254d9a932a1e6c5b75d6107922c271d
Samuel Valdes <sv1220@ic.ac.uk>
2021-08-12 15:42:15 +0100

    last updates

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template  # added render_template!
+app = Flask(__name__)                     
+    
+@app.route('/play')                           
+def play_level_one():
+    return render_template('index.html', num_times=3)
+
+@app.route('/play/<num>')                           
+def play_level_two(num):
+    return render_template('index.html', num_times=int(num)) 
+
+@app.route('/play/<num>/<color>')                           
+def play_level_three(num,color):
+    return render_template('index.html', num_times=int(num), color=color) 
+    
+if __name__=="__main__":
+    app.run(debug=True)      

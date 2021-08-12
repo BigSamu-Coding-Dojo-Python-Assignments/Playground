521f8f380112c4bef9e27fd50cca8681765b9c48
Samuel Valdes <sv1220@ic.ac.uk>
2021-08-12 16:22:40 +0100

    last updates

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template  # added render_template!
-app = Flask(__name__)                     
-    
-@app.route('/play')                           
-def play_level_one():
-    return render_template('index.html', num_times=3)
+from flask import Flask, render_template
+app = Flask(__name__)
 
-@app.route('/play/<num>')                           
-def play_level_two(num):
-    return render_template('index.html', num_times=int(num)) 
+@app.route('/')
+def normal_checkboard():
+    return render_template('index.html', num_rows=8, num_cols=8)
 
-@app.route('/play/<num>/<color>')                           
-def play_level_three(num,color):
-    return render_template('index.html', num_times=int(num), color=color) 
-    
+@app.route('/<x>')
+def sized_checkboard(x):
+    return render_template('index.html', num_rows=int(x), num_cols=int(x))
+
+@app.route('/<x>/<y>')
+def sized_checkboard_two_dimensions(x, y):
+    return render_template('index.html', num_rows=int(x), num_cols=int(y))
+
+@app.route('/<x>/<y>/<c1>/<c2>')
+def sized_checkboard_two_dimensions_and_color(x, y, c1, c2):
+    return render_template('index.html', num_rows=int(x), num_cols=int(y), color1=c1, color2=c2)
+
 if __name__=="__main__":
-    app.run(debug=True)      
+    app.run(debug=True)
